d153630a56180f573e89f8f81c0da3a01f581798
ravictation <adjiedewantara24@gmail.com>
2023-08-26 08:09:19 +0800

    add cors

--- internal/pkg/server.go
+++ internal/pkg/server.go
@@ -6,6 +6,7 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rs/cors"
 )
 
 func Server(router *gin.Engine) *http.Server {
@@ -13,13 +14,13 @@ func Server(router *gin.Engine) *http.Server {
 	if port := os.Getenv("PORT"); port != "" {
 		addr = ":" + port
 	}
-
+	corss := cors.AllowAll()
 	srv := &http.Server{
 		Addr:         addr,
 		WriteTimeout: time.Second * 15,
 		ReadTimeout:  time.Second * 15,
 		IdleTimeout:  time.Second * 15,
-		Handler:      router,
+		Handler:      corss.Handler(router),
 	}
 
 	return srv
